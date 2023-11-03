1d96044e8440532e793c93095f0d7737df8ef9a5
pravinsingh-rajput <singhpravin17a2@gmail.com>
2023-11-03 23:25:49 +0530

    Responsive on device

--- src/component/footerComp/FooterComp.js
+++ src/component/footerComp/FooterComp.js
@@ -5,25 +5,29 @@ import "./FooterComp.css";
 const FooterComp = () => {
   return (
     <footer>
-      <InstagramIcon className="instaicon" />
-      <ul className="bottomlist">
-        <li>PRESS</li>
-        <li>CAREERS</li>
-        <li>JOIN THE PACK</li>
-      </ul>
-      <div className="newsletter">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Sign up to our newsletter"
-        />
-        <p className="enterArrow">&rarr; </p>
+      <div className="footer_container_one">
+        <InstagramIcon className="instaicon" />
+        <ul className="bottomlist">
+          <li>PRESS</li>
+          <li>CAREERS</li>
+          <li>JOIN THE PACK</li>
+        </ul>
       </div>
-      <p className="copright">
-        All rights reserved Designed. developed & povvered by HOWL Digital
-        Agency
-      </p>
+      <div className="footer_container_two">
+        <div className="newsletter">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Sign up to our newsletter"
+          />
+          <p className="enterArrow">&rarr; </p>
+        </div>
+        <p className="copright">
+          All rights reserved Designed. developed & povvered by HOWL Digital
+          Agency
+        </p>
+      </div>
     </footer>
   );
 };
